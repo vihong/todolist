7218bff7a380162ae2d34b6423bfc8dc44dba76c
vihong <probamaths@gmail.com>
2019-11-16 00:12:54 +0100

    :speech_balloon: : from French to English

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ function loadList (array) {
 const options = { weekday: "long", month: "short", day: "numeric" };
 const today = new Date();
 
-dateElement.innerHTML = today.toLocaleDateString("fr-FR", options);
+dateElement.innerHTML = today.toLocaleDateString("en-UK", options);
 
 
 //--------------------------------      addToDo()    ------------------------------------------//
